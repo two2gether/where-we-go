3cd2e06ad27efa39a67e53137915f14c2727891f
hyohee0613 <gygml9@gmail.com>
2025-07-17 15:08:10 +0900

    init: 초기 커밋

---
+++ src/main/java/com/example/wherewego/WhereWeGoApplication.java
@@ -0,0 +1,13 @@
+package com.example.wherewego;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhereWeGoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhereWeGoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/wherewego/WhereWeGoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wherewego;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhereWeGoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
