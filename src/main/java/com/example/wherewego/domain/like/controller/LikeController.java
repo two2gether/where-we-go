296db7f186548c0c78211c389b872eb21a5a7514
yebitt <yebin0519@naver.com>
2025-07-22 10:15:50 +0900

    feat: 인가 추가

--- src/main/java/com/example/wherewego/domain/like/controller/LikeController.java
+++ src/main/java/com/example/wherewego/domain/like/controller/LikeController.java
@@ -1,11 +1,13 @@
 package com.example.wherewego.domain.like.controller;
 
+import com.example.wherewego.domain.auth.security.CustomUserDetail;
 import com.example.wherewego.domain.like.dto.LikeResponseDto;
 import com.example.wherewego.domain.like.service.LikeService;
 import com.example.wherewego.global.response.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -18,10 +20,10 @@ public class LikeController {
     // 코스 좋아요 등록
     @PostMapping("/courses/{courseId}/like")
     public ResponseEntity<ApiResponse<LikeResponseDto>> create(
-            @PathVariable Long courseId
+            @PathVariable Long courseId,
+            @AuthenticationPrincipal CustomUserDetail userDetails
     ) {
-        // TODO 인가 처리
-        Long userId = 1L;
+        Long userId = userDetails.getUser().getId();
         LikeResponseDto dto = likeService.create(userId, courseId);
         return new ResponseEntity<>(ApiResponse.ok( "좋아요가 등록되었습니다.", dto), HttpStatus.CREATED);
     }
@@ -29,10 +31,10 @@ public class LikeController {
     // 코스 좋아요 삭제
     @DeleteMapping("/courses/{courseId}/like")
     public ResponseEntity<ApiResponse<Void>> delete(
-            @PathVariable Long courseId
+            @PathVariable Long courseId,
+            @AuthenticationPrincipal CustomUserDetail userDetails
     ) {
-        // TODO 인가 처리
-        Long userId = 1L;
+        Long userId = userDetails.getUser().getId();
         likeService.delete(userId, courseId);
         return ResponseEntity.ok(ApiResponse.ok("좋아요가 삭제되었습니다.", null));
     }
