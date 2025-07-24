97ba14b4ccda554107db9de93a1b353d5779fdae
hyohee0613 <gygml9@gmail.com>
2025-07-24 12:57:46 +0900

    refactor: N+1문제 해결

--- src/main/java/com/example/wherewego/domain/courses/repository/CommentRepository.java
+++ src/main/java/com/example/wherewego/domain/courses/repository/CommentRepository.java
@@ -1,10 +1,8 @@
 package com.example.wherewego.domain.courses.repository;
 
-import java.util.List;
-import java.util.Optional;
-
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -15,14 +13,9 @@ import com.example.wherewego.domain.courses.entity.Comment;
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
-	List<Comment> findByCourseId(Long courseId);
-
-	Optional<Comment> findByIdAndUserId(Long commentId, Long userId);
-
 	//특정 코스에 대한 댓글 목록 최신순 조회
 	@Query("SELECT c FROM Comment c WHERE c.course.id = :courseId ORDER BY c.createdAt DESC")
+	@EntityGraph(attributePaths = {"user", "course"})
 	Page<Comment> findAllByCourseIdOrderByCreatedAtDesc(@Param("courseId") Long courseId, Pageable pageable);
-
-	boolean existsByIdAndUserId(Long commentId, Long userId);
 
 }
