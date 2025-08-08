b2f657ac5d8c67a488deee134ad83b01f2e3cf08
Geyapse <8290m@naver.com>
2025-08-08 20:53:49 +0900

    feat: EventRepositroy 에 atomic락을 위한 쿼리 설정

--- src/main/java/com/example/wherewego/domain/eventproduct/repository/EventRepository.java
+++ src/main/java/com/example/wherewego/domain/eventproduct/repository/EventRepository.java
@@ -5,6 +5,8 @@ import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import com.example.wherewego.domain.eventproduct.entity.EventProduct;
@@ -14,4 +16,15 @@ public interface EventRepository extends JpaRepository<EventProduct, Long> {
 	Optional<EventProduct> findByIdAndIsDeletedFalse(@Param("productId") Long productId);
 
 	Page<EventProduct> findAllByIsDeletedFalse(Pageable pageable);
+
+	@Modifying
+	@Query("""
+		UPDATE EventProduct e
+		   SET e.stock = e.stock - :quantity
+		 WHERE e.id = :productId
+		   AND e.isDeleted = false
+		   AND e.stock >= :quantity
+		""")
+	int decreaseStockIfAvailable(@Param("productId") Long productId,
+		@Param("quantity") int quantity);
 }
