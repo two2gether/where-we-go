bbc719a8998b0ce7c7435dbc529f45b322c79a59
hyohee0613 <gygml9@gmail.com>
2025-08-08 18:17:08 +0900

    refactor: UserRepository 의존을 UserService로 변경

--- src/test/java/com/example/wherewego/domain/course/service/CommentServiceTest.java
+++ src/test/java/com/example/wherewego/domain/course/service/CommentServiceTest.java
@@ -27,8 +27,9 @@ import com.example.wherewego.domain.courses.entity.Course;
 import com.example.wherewego.domain.courses.repository.CommentRepository;
 import com.example.wherewego.domain.courses.repository.CourseRepository;
 import com.example.wherewego.domain.courses.service.CommentService;
+import com.example.wherewego.domain.courses.service.NotificationService;
 import com.example.wherewego.domain.user.entity.User;
-import com.example.wherewego.domain.user.repository.UserRepository;
+import com.example.wherewego.domain.user.service.UserService;
 import com.example.wherewego.global.exception.CustomException;
 import com.example.wherewego.global.response.PagedResponse;
 
@@ -43,10 +44,13 @@ class CommentServiceTest {
 	private CommentRepository commentRepository;
 
 	@Mock
-	private UserRepository userRepository;
+	private UserService userService;
 
 	@Mock
 	private CourseRepository courseRepository;
+
+	@Mock
+	private NotificationService notificationService;
 
 	private User user;
 	private Course course;
@@ -75,7 +79,7 @@ class CommentServiceTest {
 			// given
 			CommentRequestDto requestDto = new CommentRequestDto("댓글 내용");
 
-			given(userRepository.findByIdAndIsDeletedFalse(1L)).willReturn(Optional.of(user));
+			given(userService.getUserById(1L)).willReturn(user);
 			given(courseRepository.findByIdWithThemes(10L)).willReturn(Optional.of(course));
 			given(commentRepository.save(any(Comment.class)))
 				.willAnswer(invocation -> invocation.getArgument(0));
@@ -101,7 +105,7 @@ class CommentServiceTest {
 
 			CommentRequestDto requestDto = new CommentRequestDto("비공개 댓글");
 
-			given(userRepository.findByIdAndIsDeletedFalse(1L)).willReturn(Optional.of(user)); //현재 로그인된 사용자는 id=1
+			given(userService.getUserById(1L)).willReturn(user); //현재 로그인된 사용자는 id=1
 			given(courseRepository.findByIdWithThemes(20L)).willReturn(Optional.of(privateCourse));
 
 			// when & then
@@ -177,6 +181,8 @@ class CommentServiceTest {
 				Comment.builder().id(2L).content("댓글2").user(user).course(course).build()
 			);
 			Page<Comment> commentPage = new PageImpl<>(comments, pageable, comments.size());
+
+			given(courseRepository.findById(10L)).willReturn(Optional.of(course));
 
 			given(commentRepository.findAllByCourseIdOrderByCreatedAtDesc(10L, pageable)).willReturn(commentPage);
 
