a3838c386568c586bb1da9aec6278b5a7f1d4a64
balsohn <balsohn@naver.com>
2025-08-19 00:18:01 +0900

    fix: Jakarta EE import 수정

--- src/main/java/com/example/wherewego/global/controller/HealthController.java
+++ src/main/java/com/example/wherewego/global/controller/HealthController.java
@@ -5,7 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletRequest;
 import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
