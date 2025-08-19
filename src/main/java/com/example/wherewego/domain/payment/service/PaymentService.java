12cc7a9431e488cd6e842fa9f2325118f9bf663e
balsohn <balsohn@naver.com>
2025-08-19 23:00:17 +0900

    debug: 토스 콜백 설정 로그 추가
    - resultCallback과 callbackVersion 값 로깅
    - 토스 API에 전달되는 콜백 설정 확인용

--- src/main/java/com/example/wherewego/domain/payment/service/PaymentService.java
+++ src/main/java/com/example/wherewego/domain/payment/service/PaymentService.java
@@ -127,6 +127,8 @@ public class PaymentService {
 
 		log.info("토스 결제 API 요청 - orderNo: {}, amount: {}, apiKey 설정됨: {}", 
 			tossRequestDto.getOrderNo(), tossRequestDto.getAmount(), tossRequestDto.getApiKey() != null);
+		log.info("토스 콜백 설정 - resultCallback: {}, callbackVersion: {}", 
+			tossRequestDto.getResultCallback(), tossRequestDto.getCallbackVersion());
 
 		// 5. WebClient로 토스 결제 API POST 요청
 		PaymentResponseDto responseDto = tossWebClient.post()
